1855e08a3960f4dd824056ae3a025b82d6514acf
Odii Daniel <deoscomputers@gmail.com>
2022-12-29 15:51:32 +0100

    add new version

--- script.js
+++ script.js
@@ -1,148 +1,105 @@
-const insertBtn = document.getElementById("addCourse");
-const courseRow = document.getElementById("courseRow");
-const panel = document.getElementById("key");
-const Box = document.getElementById("master-row");
 
-const newRow = document.getElementsByClassName("new-row");
-const cascade = document.getElementsByClassName("delete");
+const GPA_DISPLAY = document.getElementById("gpa-display");
+const display = document.getElementById("calculator-display");
+const user_choice = document.getElementById("input");
+const calculate_btn = document.getElementById("calculate_btn");
 
-const unit = document.getElementsByClassName("units");
+let box = document.createElement("form");
+box.className = "gpa-box";
+box.onsubmit =()=>{return false};
+let counter = 0;
 
-const grades = document.getElementsByClassName("grades");
-
-const selector =  document.getElementsByClassName("select");
-const display = document.getElementById("display");
-
-let counter;
-
-let userUnits = [];
-let userGrades = [];
-
-let i = 0;
-
-let keyPoints = {"A":5, "B":4, "C":3, "D":2, "E":1, "F":0};
-
-
-counter = 0;
-
-insertBtn.onclick =()=>{
-      // panel.innerHTML += Box.innerHTML;
-
-
-    let box = document.createElement("div");
-    box.innerHTML =  `
-            <div class="new-row block" id="courseRow">
-            <input class="select course" type="text" placeholder="Course" id="courseRow${counter}">
+function createInstance(){
+    box.innerHTML = "";
+    for(let i = 0; i < user_choice.value; i++){
+        counter++;
+        box.innerHTML +=  
+        `<input class="select course" type="text" placeholder="Course code" id="courseRow${counter}" required="true">
+        
+        <input class="select units" type="number" placeholder="Course units" id="unitRow${counter}" required="true">
             
-            <input class="select units" type="number" placeholder="units" id="unitRow${counter}">
-                
-            <select class="select grades" name="grade" id="gradeRow${counter}">
-                <option value="">grade</option>
-                <option value="5">A</option>
-                <option value="4">B</option>
-                <option value="3">C</option>
-                <option value="2">D</option>
-                <option value="1">E</option>
-                <option value="0">F</option>
-            </select>
-
-            <div class="remover"><i class="bi bi-trash-fill delete"></i></div>
-            </div>
-    `;
-    panel.appendChild(box);
-    //initiate counter to control arrays...
-    
-
- 
-  
-  
-    
-//log the number of grade rows inserted by the user on the console.....
-    for(let i = 0; i < grades.length; i++){
-    console.log(grades.length);
+        <select class="select grades" name="grade" id="gradeRow${counter}" required="true">
+            <option value="">grade</option>
+            <option value="5">A</option>
+            <option value="4">B</option>
+            <option value="3">C</option>
+            <option value="2">D</option>
+            <option value="1">E</option>
+            <option value="0">F</option>
+        </select>
+        <br/> 
+        <div class="divider"></div>
+        `;
     }
+    box.innerHTML += `<input type="submit" value="submit" class="chi-btn" onclick="getGPA()">`;
+    display.appendChild(box);
+};
 
-//automatically set an indexed unit load for each new row of record...
+createInstance();
+
+input.onchange =()=>{
+    createInstance();
+    console.log(input.value);
     for(let i = 0; i < unit.length; i++){
-       // unit[i].setAttribute("id", `unitLoad${counter++}`);
-        unit[i].onchange =()=>{
-            // console.log("value for a row has changed");
-            // console.log("value for row " + document.getElementById(`unitLoad${i}`).value + " has changed!")
-        }
-    }
-
-    //DELETING FUNTIONS>........
-    //initiate delete ability for each new row created by user...
-    for(let i = 0; i < cascade.length; i++){
-    cascade[i].onclick =()=>{
-            newRow[i].remove();
-            console.log(i + " has been removed!");
-            userUnits.splice(document.getElementById(`unitRow${cascade[i]}`), 1);
-            console.log("remaining items in units array are: " + userUnits);
-        }
-    }
-    
-    //alert user on new row added...
-    console.log("new course added!")
-
-counter += 1;
-
+     //check if theres any input left empty....
+     if(unit[i].value != "" && unit[i].value != null && unit[i].value != " "){ 
+        calculate_btn.onclick =()=>{getGPA()}
+        calculate_btn.style.background = "red";
+    };
 }
+   }
+    
 
-
-const multiplex = []
-let Toptal, totalMultiplex;
+let multiplex, Toptal, totalMultiplex, userUnits, userGrades;
+const unit = document.getElementsByClassName("units");
+const grades = document.getElementsByClassName("grades");
+const courses = document.getElementsByClassName("course");
 
 function getGPA(){
-    
-for(let i = 0; i < unit.length; i++){
 
+    //initially set all arrays empty to avoid auto caching giving wrong values...
+    userUnits = [];
+    userGrades = [];
+    multiplex = [];
+
+   
+
+
+
+    //loop through all input rows and perform calculations...........
+for(let i = 0; i < unit.length; i++){
     
+    //form should ot be submittd while an input is left empty
+if(unit[i].value > 0 && grades[i].value != 0 && courses[i].value != ""){
     userUnits.push(parseInt(unit[i].value));
     userGrades.push(grades[i].value);
-   
     Toptal = userUnits[i] * userGrades[i];
-
-
     // const sumOfUnits = userUnits.reduce((a,b) => a + b, 0);
     const sumOfGrades = userGrades.reduce((a,b) => a + b, 0);
-    
-
     multiplex.push(Toptal);
-
     totalMultiplex = multiplex.reduce((a,b) => a + b, 0)
-
     console.log(totalMultiplex);
+
+
+    //error logging to monitor the system via console.......
+    console.log("the units array: " + userUnits);
+    console.log("the grades array: " + userGrades);
+    console.log("GPA: " + totalMultiplex/userUnits.reduce((a,b) => a + b, 0))
+    GPA_DISPLAY.innerHTML = totalMultiplex/userUnits.reduce((a,b) => a + b, 0);
+    GPA_DISPLAY.innerHTML = parseFloat(GPA_DISPLAY.innerHTML.slice(0,4)).toFixed(2);
+}
+}
 }
 
-console.log("the units array: " + userUnits);
-console.log("the grades array: " + userGrades);
+//this function is still under test and would be worked on consequently XD
+//DELETING FUNTIONS>........
+    //initiate delete ability for each new row created by user...
 
-
-console.log("GPA: " + totalMultiplex/userUnits.reduce((a,b) => a + b, 0))
-display.innerHTML = totalMultiplex/userUnits.reduce((a,b) => a + b, 0);
-display.innerHTML = parseFloat(display.innerHTML.slice(0,4)).toFixed(2);
-// if(display.innerHTML.length > 1){display.innerHTML += ".0"};
-
-console.log("modified: " + display.innerHTML.slice(0,4));
-
-
-/*
-userUnits = [];
-userGrades = [];
-*/
-}
-
-
-
-for(let i = 0; i < grades.length; i++){
-    units[i].value.onchange =()=>{
-        console.log("calculae GPA...")
-        getGPA();
-    }
-}
-
-
-
-
-
+    // for(let i = 0; i < cascade.length; i++){
+    //     cascade[i].onclick =()=>{
+    //             newRow[i].remove();
+    //             console.log(i + " has been removed!");
+    //             userUnits.splice(document.getElementById(`unitRow${cascade[i]}`), 1);
+    //             console.log("remaining items in units array are: " + userUnits);
+    //         }
+    //     }
